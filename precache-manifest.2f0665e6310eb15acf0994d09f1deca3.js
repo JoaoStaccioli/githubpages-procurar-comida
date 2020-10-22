07f8cbcc128372cfe526a4d06fc9257749fe3cae
JoaoStaccioli <stacciolijoao@gmail.com>
2020-10-22 13:12:12 -0300

    Updates

---
+++ precache-manifest.2f0665e6310eb15acf0994d09f1deca3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "533e4a654e8b19ec9e2a0f7764ac049b",
+    "url": "/githubpages-procurar-comida/index.html"
+  },
+  {
+    "revision": "03d7d5790926a45f1517",
+    "url": "/githubpages-procurar-comida/static/css/main.b9d31ab8.chunk.css"
+  },
+  {
+    "revision": "46aa843389b83ab6d33d",
+    "url": "/githubpages-procurar-comida/static/js/2.5782c9fe.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/githubpages-procurar-comida/static/js/2.5782c9fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03d7d5790926a45f1517",
+    "url": "/githubpages-procurar-comida/static/js/main.77d1defc.chunk.js"
+  },
+  {
+    "revision": "fbd6441e80d176923e85",
+    "url": "/githubpages-procurar-comida/static/js/runtime-main.9b668e0d.js"
+  }
+]);
